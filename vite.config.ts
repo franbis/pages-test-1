e03b3745845c21fc5ca81f207e0e56b3da96713f
Francesco Biscazzo <11650573+franbis@users.noreply.github.com>
2026-01-29 00:50:45 +0100

    Log the base public path environment variable in the *Vite* configuration to debug issues with the reading of environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 
 
+console.log('VITE_BASE:', process.env.VITE_BASE);
+
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.VITE_BASE || '/',
